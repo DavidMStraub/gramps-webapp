3e994cc3036775439ce49769f024a172afa18e0c
David Straub <david.straub@tum.de>
2019-01-09 09:56:00 +0100

    First working version: list of people from Gramps DB

--- grampsrest/api.py
+++ grampsrest/api.py
@@ -1,11 +1,53 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_restful import reqparse,  Api, Resource
+from .db import Db
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+tree = Db('Straub')
+tree.open()
 
-api.add_resource(HelloWorld, '/')
+
+parser = reqparse.RequestParser()
+parser.add_argument('fields', type=str)
+
+
+from gramps.gen.display.name import NameDisplay
+nd = NameDisplay()
+
+
+def person_to_dict(p):
+    return {
+    'gramps_id': p.gramps_id,
+    'name_display': nd.display(p),
+    'name_given': nd.display_given(p),
+    'name_surname': p.primary_name.get_surname(),
+    }
+
+
+def get_people(db, gids=None):
+    db = tree.dbstate.db
+    if gids is None:
+        return [person_to_dict(p) for p in db.iter_people()]
+    return [person_to_dict(db.get_person_from_gramps_id(gid)) for gid in gids]
+
+
+class Person(Resource):
+    def get(self, gid):
+        args = parser.parse_args()
+        gids = gid.split(',')
+        db = tree.dbstate.db
+        return get_people(db, gids)
+
+
+class People(Resource):
+    def get(self):
+        db = tree.dbstate.db
+        return get_people(db)
+
+
+api.add_resource(People, '/people')
+api.add_resource(Person, '/people/<gid>')
---
+++ grampsrest/db.py
@@ -0,0 +1,21 @@
+from gramps.cli.clidbman import CLIDbManager
+from gramps.gen.dbstate import DbState
+from gramps.cli.grampscli import CLIManager
+from gramps.cli.user import User
+import os
+
+
+class Db():
+    def __init__(self, name):
+        self.dbstate = DbState()
+        self.dbman = CLIDbManager(self.dbstate)
+        self.user = User()
+        self.smgr = CLIManager(self.dbstate, True, self.user)
+        self.path = self.dbman.get_family_tree_path('Straub')
+
+    def is_locked(self):
+        return os.path.isfile(os.path.join(self.path, "lock"))
+
+    def open(self):
+        self.dbman.break_lock(self.path)
+        self.smgr.open_activate(self.path)
