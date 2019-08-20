e1de12205486a1b9bd505f254dda21058e75b3d0
David Straub <david.straub@tum.de>
2019-08-20 20:51:47 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from setuptools import setup, find_packages
+import glob
 
 
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 
+PACKAGE_DATA = [p[14:] for p in glob.glob('gramps_webapp/js/**/*', recursive=True)]
+
+
 setup(name='gramps-webapp',
       version='0.1',
       author='David M. Straub <david.straub@tum.de>',
+      author_email='david.straub@tum.de',
+      url='https://github.com/DavidMStraub/gramps-webapp',
       long_description=LONG_DESCRIPTION,
       long_description_content_type='text/markdown',
       license='GPLv3',
       packages=find_packages(),
+      package_data={'gramps_webapp': PACKAGE_DATA},
       install_requires=['flask',
                         'flask-restful',
                         'flask-caching',
